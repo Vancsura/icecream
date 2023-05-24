2d6a633ff9c9796368f22ebb24637cd27b521f47
Vancsura László <valaci1992@gmail.com>
2023-05-24 13:38:43 +0200

    Image element customization

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -2,20 +2,12 @@ import React from 'react';
 import './css/App.css';
 
 import { Button } from 'react-bootstrap';
+import Navbar from "./Navbar";
 
 function App() {
   return (
       <div>
-        <>
-          <h1>Bootstrap 5 with React</h1>
-          <Button variant="primary">Primary</Button>{' '}
-          <Button variant="secondary">Secondary</Button>{' '}
-          <Button variant="success">Success</Button>{' '}
-          <Button variant="warning">Warning</Button>{' '}
-          <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
-          <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
-          <Button variant="link">Link</Button>
-        </>
+          <Navbar />
       </div>
   );
 }
--- react-frontend/src/Navbar.tsx
+++ react-frontend/src/Navbar.tsx
@@ -3,7 +3,7 @@ export default function Navbar () {
            // <!-- Top menu -->
             <nav className="navbar navbar-dark fixed-top navbar-expand-md navbar-no-bg">
                 <div className="container">
-                    <a className="navbar-brand" href="index.html">Bootstrap 4 Carousel Fullscreen Template</a>
+                    <a className="navbar-brand" href="index.html"></a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
