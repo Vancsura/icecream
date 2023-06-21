35b61069affcb773dc15195e67859254295c22f1
Vancsura <valaci1992@gmail.com>
2023-06-21 08:54:02 +0200

    Start page style is done, navigation implementation needed.

--- react-frontend/src/components/Navbar.tsx
+++ react-frontend/src/components/Navbar.tsx
@@ -1,42 +1,24 @@
-import React, {useState} from 'react';
-import {SplitButton} from "react-bootstrap";
-
 export default function Navbar() {
-
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-    };
 
     return (
         <nav className="navbar fixed-top navbar-expand-md">
 
-            <div className="navbar-collapse collapse w-100 order-1 order-md-0">
+            <div className="container">
                 <ul className="navbar-nav mr-auto">
                     <li>
                         <a className="nav-link" href="/open">Nyitvatartás</a>
                     </li>
                 </ul>
-                <a className="navbar-brand center mx-auto" href="/">
+
+                <div className="navbar-brand mx-auto order-0">
                     <img src="https://picsum.photos/800" alt="Nem ez kell"/>
-                </a>
+                </div>
                 <ul>
                     <li>
-                        <a className="nav-link" href="/order">Rendelés</a>
+                        <a className="nav-link mx-6" href="/order">Rendelés</a>
                     </li>
                 </ul>
             </div>
-            {/*<div className="dropdown me-auto">*/}
-            {/*    <button className="dropbtn navbar-toggler collapsed me-auto mb-5 pb-3"*/}
-            {/*            style={{float: 'right'}}>*/}
-            {/*        <i className="bi bi-menu-down" style={{color: 'white'}}></i>*/}
-            {/*    </button>*/}
-            {/*    <div className="dropdown-content" style={{right: 0}}>*/}
-            {/*        <a href="#">Nyitvatartás</a>*/}
-            {/*        <a href="#">Rendelés</a>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
         </nav>
     );
 }
